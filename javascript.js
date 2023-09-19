49a049a0b7b939bff339595d5526d0d223bd6e75
Nishavishwanathan03 <132324991+Nishavishwanathan03@users.noreply.github.com>
2023-09-19 12:09:31 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+console.log("Gettingstartedwithjs");
+console.warn("How are you?");
+console.error("ERRROR");
+//1.)programming
+//A.)Variables
+//declaration
+//initializaton
+var age;
+age = 24;
+var bag = "books";
+var fullname = "sidharthan";
+bag = "tiffenbox";
+var count = "20";
+console.log(age);
+console.log(count);
+console.log(bag);
+console.log(fullname);
+var fname = "Nisha";
+var lname = "V";
+console.log(fname + lname);
+//2.)Handling your views
+//---> for controlling your views
