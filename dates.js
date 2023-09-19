a7c5647f576a1d5d24d0675511b54947cb773188
Nishavishwanathan03 <132324991+Nishavishwanathan03@users.noreply.github.com>
2023-09-19 12:15:01 +0530

    Add files via upload

---
+++ dates.js
@@ -0,0 +1,13 @@
+var currentDT = new Date
+var currentTimeUnix = currentDT.getTime();
+var currrentFullYr = currentDT.getFullYear();
+var currentDay = currentDT.getDate();
+var currentMonth = currentDT.getMonth();
+var currentDateString = currentDT.toDateString();
+console.log(currentTimeUnix);
+console.log(currrentFullYr);
+console.log(currentDay);
+console.log(currentMonth);
+console.log(currentDateString);
+
+
