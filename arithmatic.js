5341e79417d00cb9d4965af65261ef2fdfa89023
Nishavishwanathan03 <132324991+Nishavishwanathan03@users.noreply.github.com>
2023-09-19 14:01:55 +0530

    Add files via upload

---
+++ arithmatic.js
@@ -0,0 +1,12 @@
+var english = 50;
+var maths = 79;
+var science = 82;
+var arts = 82;
+var physics = 99;
+var total = 100;
+console.log(english + maths + science + arts);
+console.log(total - arts);
+console.log(total * 20);
+console.log(total / 10);
+console.log(total % 2);
+
