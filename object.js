df7a2efb7adf94ee551e80e19e741e4a41343ffd
Nishavishwanathan03 <132324991+Nishavishwanathan03@users.noreply.github.com>
2023-09-26 09:53:39 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,9 @@
+const person = {}
+
+person["firstname"] = "Nisha" ;
+person["Lastname"] = "Vishwanadhan"
+person.age = "20";
+person.designation = "Fullstack Developer";
+person.address = "dkfjeroijfev";
+console.log(person);
+
